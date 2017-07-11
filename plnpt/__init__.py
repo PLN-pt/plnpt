76958bcda15535eeae63d8ffc28518bca6f2f287
Nuno Carvalho <mestre.smash@gmail.com>
2017-07-11 10:06:12 +0100

    Add stopwords and term frquency methods

--- plnpt/__init__.py
+++ plnpt/__init__.py
@@ -5,8 +5,8 @@ import json
 
 API = 'http://api.pln.pt/'
 
-def tokenizer(text):
-    opts = ''
+def tokenizer(text, sentence=0):
+    opts = 'sentence='+str(sentence)
     return _proc('tokenizer', opts, text)
 
 def tagger(text, ner=0):
@@ -17,11 +17,21 @@ def dep_parser(text):
     opts = ''
     return _proc('dep_parser', opts, text)
 
+def tf(text):
+    opts = ''
+    return _proc('tf', opts, text)
+
+def stopwords():
+    opts = ''
+    return _proc('stopwords', opts, None)
+
 def _proc(action, opts, text):
-	url = API + action + '?' + opts
-	data = text.encode('utf8')
-	req = urllib.request.Request(url, data)
-	with urllib.request.urlopen(req) as response:
-		p = response.read()
-	return json.loads(p)
+    url = API + action + '?' + opts
+    data = None
+    if (text):
+        data = text.encode('utf8')
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as response:
+        p = response.read()
+    return json.loads(p)
 
